6dd2747e7a4842efdce2e15dee04054da5bd567a
IMCueto <cuetosalazarsebas@gmail.com>
2022-12-18 00:21:04 -0500

    a

--- crudo/settings.py
+++ crudo/settings.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+import dj_database_url
 """
 Django settings for crudo project.
 
@@ -10,9 +13,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
-import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
