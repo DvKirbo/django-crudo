d560d1857b70879a08c9f8c5f6f62a6adee3944d
IMCueto <cuetosalazarsebas@gmail.com>
2022-12-19 14:26:51 -0500

    final 2

--- crudo/settings.py
+++ crudo/settings.py
@@ -151,5 +151,5 @@ EMAIL_BAKCEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "cuetosalazarsebas@gmail.com"
-EMAIL_HOST_PASSWORD ="wwgnkeoilziwrrui"
+EMAIL_HOST_PASSWORD ="tayxmigbilaithta"
 EMAIL_USE_TLS = True
